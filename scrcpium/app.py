e0817f91a0268ae41480addb57385b851fbe6cd2
skylerlee <skyler.ac.lee@gmail.com>
2024-07-26 01:41:23 +0800

    feat: add first api

--- scrcpium/app.py
+++ scrcpium/app.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 
+from .routers import devices
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(devices.router, prefix="/devices")
 
 
 @app.get("/")
