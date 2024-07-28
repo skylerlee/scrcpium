5e3cb1b1e20a786227d4bde552d527945ed3ae16
skylerlee <skyler.ac.lee@gmail.com>
2024-07-28 13:30:01 +0800

    feat: add api

--- scrcpium/routers/devices.py
+++ scrcpium/routers/devices.py
@@ -1,8 +1,14 @@
+from adbutils import AdbDeviceInfo, adb
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
 @router.get("/")
-def list_devices():
-    return []
+def list_devices() -> list[AdbDeviceInfo]:
+    return adb.list()
+
+
+@router.get("/{serial}")
+def get_device(serial: str):
+    return adb.device(serial)
