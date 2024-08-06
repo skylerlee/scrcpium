989294217e630fa7e45f3b00e4b51b4529bf8b09
skylerlee <skyler.ac.lee@gmail.com>
2024-08-07 01:00:23 +0800

    fix: fix build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,12 @@ import type { UserConfig } from 'vite';
 export default {
   build: {
     outDir: '../static',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
   },
 } satisfies UserConfig;
