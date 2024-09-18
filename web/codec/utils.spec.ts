65d56d906ddfa27756f2ec8f5e0d5ed499132dc9
skylerlee <skyler.ac.lee@gmail.com>
2024-09-19 01:12:18 +0800

    test: update test

--- web/codec/utils.spec.ts
+++ web/codec/utils.spec.ts
@@ -6,8 +6,9 @@ describe('annexBSplitNalu', () => {
     const buf = new Uint8Array([
       0x00, 0x00, 0x00, 0x01, 0x01, 0x02, 0x00, 0x00, 0x00, 0x01, 0x03, 0x04,
     ]);
-    for (const seg of annexBSplitNalu(buf)) {
-      console.log(seg);
-    }
+    const chunks = Array.from(annexBSplitNalu(buf));
+    expect(chunks.length).toBe(2);
+    expect(chunks[0]).toEqual(new Uint8Array([0x01, 0x02]));
+    expect(chunks[1]).toEqual(new Uint8Array([0x03, 0x04]));
   });
 });
